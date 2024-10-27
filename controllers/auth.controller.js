df014d2d38e272ba967fada58a74201ecfa191f4
scKim <lansco1229@gmail.com>
2024-10-27 14:23:23 +0900

    add token login and logout

--- controllers/auth.controller.js
+++ controllers/auth.controller.js
@@ -1,23 +1,43 @@
+const jwt = require("jsonwebtoken")
 const User = require("../models/User")
 const bcrypt = require("bcryptjs")
+require("dotenv").config()
+const JWT_SECRET_KEY = process.env.JWT_SECRET_KEY
 
 const authController = {}
 
-authController.loginWithEmail = async(req, res) =>{
-    try{
-        const {email, password} = req.body
-        let user = await User.findOne({email})
-        if (user){
+authController.loginWithEmail = async (req, res) => {
+    try {
+        const { email, password } = req.body
+        let user = await User.findOne({ email })
+        if (user) {
             const isMatch = await bcrypt.compare(password, user.password)
-            if(isMatch){
+            if (isMatch) {
                 // token
                 const token = await user.generateToken()
-                return res.status(200).json({status:"success", user, token})
+                return res.status(200).json({ status: "success", user, token })
             }
         }
         throw new Error("invalid email or password")
-    }catch(error){
-        res.status(400).json({status:"fail", error: error.message})
+    } catch (error) {
+        res.status(400).json({ status: "fail", error: error.message })
+    }
+}
+
+authController.authenticate = async (req, res, next) => {
+    try {
+        const tokenString = req.headers.authorization   // api.js (FE)에서 정의한 api header 참고
+        if (!tokenString) {
+            throw new Error("Token not found")
+        }
+        const token = tokenString.replace("Bearer ", "")
+        jwt.verify(token, JWT_SECRET_KEY, (error, payload) => {
+            if (error) throw new Error("invalid token")
+            req.userId = payload._id   
+        })
+        next();
+    } catch (error) {
+        res.status(400).json({ status: "fail", error: error.message })
     }
 }
 
